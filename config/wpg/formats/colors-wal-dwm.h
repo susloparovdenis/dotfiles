edd0280d667cfc14c479e7d8538ce2c1628bb7fd
Denis Susloparov <densus@gmail.com>
2019-02-14 14:42:27 +0100

    work

--- config/wpg/formats/colors-wal-dwm.h
+++ config/wpg/formats/colors-wal-dwm.h
@@ -1,14 +1,14 @@
-static const char norm_fg[] = "#feffff";
-static const char norm_bg[] = "#252525";
-static const char norm_border[] = "#3e3e3e";
+static const char norm_fg[] = "#d0ffff";
+static const char norm_bg[] = "#04090F";
+static const char norm_border[] = "#0b1b2f";
 
-static const char sel_fg[] = "#feffff";
-static const char sel_bg[] = "#deb887";
-static const char sel_border[] = "#feffff";
+static const char sel_fg[] = "#d0ffff";
+static const char sel_bg[] = "#576974";
+static const char sel_border[] = "#d0ffff";
 
-static const char urg_fg[] = "#feffff";
-static const char urg_bg[] = "#ef6769";
-static const char urg_border[] = "#ef6769";
+static const char urg_fg[] = "#d0ffff";
+static const char urg_bg[] = "#4AA6E8";
+static const char urg_border[] = "#4AA6E8";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
