69c8c2864715b6be0b20f633b2ef06fdfd4a1abd
Denis Susloparov <densus@gmail.com>
2019-05-04 20:39:55 +0200

    ssh

--- excluded/004-general-pkg.sh
+++ excluded/004-general-pkg.sh
@@ -2,4 +2,4 @@
 yay -S --noconfirm \ 
     zsh chromium telegram-desktop mpv code thefuck antigen lsd \
     chromium-widevine ranger spotify rcm docker docker-compose antigen-git trash-cli \
-    xorg-xkill xorg-xprop cht.sh neofetch nerd-fonts-dejavu-complete flameshot fzf fzy emacs neovim
+    xorg-xkill xorg-xprop cht.sh neofetch nerd-fonts-dejavu-complete flameshot fzf fzy emacs neovim highlight
