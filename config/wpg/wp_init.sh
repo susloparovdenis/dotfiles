3f916e52623119611215db367af887d54db6f69f
Denis Susloparov <densus@gmail.com>
2019-05-07 18:46:52 +0200

    s

--- config/wpg/wp_init.sh
+++ config/wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs 'DNA_3_d_structure_molecule_pattern_abstraction_genetic_psychedelic_3000x2250.jpg' 'DNA_3_d_structure_molecule_pattern_abstraction_genetic_psychedelic_3000x2250.jpg'
+wpg -rs 'Atlantis_Nebula__3_by_Starkiteckt_15200x9500.jpg' 'Atlantis_Nebula__3_by_Starkiteckt_15200x9500.jpg'
